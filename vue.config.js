bd5f16051b52b3e053e254c0994a28d5d08649f5
Denisoed <denisoed@gmail.com>
2021-02-05 09:55:40 +0600

    minor fix for pablicPath in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
-const detectPath = () => {
+/**
+ * If you want to use Gitlab pages,
+ * you need to adjust the path for your project.
+ * 
+ * Please see this documentation https://cli.vuejs.org/ru/guide/deployment.html#gitlab-pages
+ */
+const gitlabPagePath = () => {
+  const path = new URL(process.env.CI_PROJECT_URL).pathname;
   if (process.env.NODE_ENV === 'production') {
-    if (process.env.CI_PROJECT_PATH !== undefined) {
-      return '/' + process.env.CI_PROJECT_PATH + '/';
+    if (path) {
+      return '/' + path + '/'; 
     }
     return '/';
   }
@@ -9,5 +16,5 @@ const detectPath = () => {
 };
 
 module.exports = {
-  publicPath: detectPath()
+  publicPath: gitlabPagePath()
 };
