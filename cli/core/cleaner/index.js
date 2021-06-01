bf55806308275c3b21abc106b34504630e7893da
Denisoed <denisoed@gmail.com>
2021-06-01 12:26:40 +0600

    Added block for remove cypress files & folders

--- cli/core/cleaner/index.js
+++ cli/core/cleaner/index.js
@@ -2,21 +2,33 @@ const packageJson = require('./packageJson');
 const fs = require('fs');
 const path = require('path');
 
-const name = 'package.json';
-const dist = path.resolve();
+const root = path.resolve();
+
+function removeFolder(folder) {
+  fs.rmdirSync(folder, { recursive: true });
+}
 
 function removeFile(file) {
   fs.unlink(file, err => { if (err) throw err; });
 }
 
-function writeFile(data) {
-  fs.writeFileSync(`${dist}/${name}`, data, err => { if (err) throw err; });
+function writeFile(data, file) {
+  fs.writeFileSync(file, data, err => { if (err) throw err; });
 }
 
 function run(options) {
   // Update package.json
   const newPackageJson = packageJson.update(options);
-  writeFile(JSON.stringify(newPackageJson, null, 2));
+  writeFile(JSON.stringify(newPackageJson, null, 2), `${root}/package.json`);
+
+  // Remove cypress
+  if (options.cypress) {
+    removeFile(`${root}/docker-compose.e2e.yml`);
+    removeFile(`${root}/cy-open.yml`);
+    removeFile(`${root}/cypress.json`);
+    removeFolder(`${root}/tests/e2e`);
+    removeFolder(`${root}/tests/server`);
+  }
 }
 
 module.exports = { run }
